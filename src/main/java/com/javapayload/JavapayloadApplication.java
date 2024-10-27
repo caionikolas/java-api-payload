613dee719808cb05128d3b4c3cc2c0ac94e0493e
caionikolas <Caio.kanu@hotmail.com>
2024-10-27 14:33:47 -0300

    feat: initial config

---
+++ src/main/java/com/javapayload/JavapayloadApplication.java
@@ -0,0 +1,13 @@
+package com.javapayload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapayloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavapayloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javapayload/JavapayloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javapayload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavapayloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
